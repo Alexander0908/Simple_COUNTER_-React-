89da3660cae47e2598068854383ca1140d559f92
Alex <example@mail.com>
2021-10-09 11:06:06 +0300

    first commit with the whole project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+  nextCount = () => {
+    this.setState(state => ({ 
+      count: state.count + 1
+    }))
+  }
+  prevCount = () => {
+    this.setState(state => ({ 
+      count: state.count - 1
+    }))
+  }
+  
+  rndCount = () => {
+    this.setState({
+      count: +(Math.random() * (50 - -50) + -50).toFixed(0)
+    })
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  resetCount = () => {
+    this.setState({
+      count: this.state.count = 0
+    })
+  }
+
+  
+  render() {
+    const {count} = this.state;
+    
+    return (
+      <div class="app">
+        <div class="counter">{count}</div>
+        <div class="controls">
+          <button onClick={this.nextCount}>INC</button>
+          <button onClick={this.prevCount}>DEC</button>
+          <button onClick={this.rndCount}>RND</button>
+          <button onClick={this.resetCount}>RESET</button>
+        </div>
+      </div>
+    )
+  }
 }
+
+//ReactDOM.render(<App counter={0}/>, document.getElementById('app'));
 
 export default App;
